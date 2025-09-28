9d82c69c279473fdaed21fd12d26cdd72777f352
Yusuf52Ah <yusufxon5225@gmail.com>
2025-09-28 11:13:28 +0500

    First

---
+++ main.js
@@ -0,0 +1,240 @@
+const products = [
+    {
+        "id": 38,
+        "title": "Vibrant Runners: Bold Orange & Blue Sneakers",
+        "slug": "vibrant-runners-bold-orange-blue-sneakers",
+        "price": 27,
+        "description": "Step into style with these eye-catching sneakers featuring a striking combination of orange and blue hues. Designed for both comfort and fashion, these shoes come with flexible soles and cushioned insoles, perfect for active individuals who don't compromise on style. The reflective silver accents add a touch of modernity, making them a standout accessory for your workout or casual wear.",
+        "category": {
+            "id": 4,
+            "name": "Shoes",
+            "slug": "shoes",
+            "image": "https://i.imgur.com/qNOjJje.jpeg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/hKcMNJs.jpeg",
+            "https://i.imgur.com/NYToymX.jpeg",
+            "https://i.imgur.com/HiiapCt.jpeg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 39,
+        "title": "Vibrant Pink Classic Sneakers",
+        "slug": "vibrant-pink-classic-sneakers",
+        "price": 84,
+        "description": "Step into style with our Vibrant Pink Classic Sneakers! These eye-catching shoes feature a bold pink hue with iconic white detailing, offering a sleek, timeless design. Constructed with durable materials and a comfortable fit, they are perfect for those seeking a pop of color in their everyday footwear. Grab a pair today and add some vibrancy to your step!",
+        "category": {
+            "id": 4,
+            "name": "Shoes",
+            "slug": "shoes",
+            "image": "https://i.imgur.com/qNOjJje.jpeg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/mcW42Gi.jpeg",
+            "https://i.imgur.com/mhn7qsF.jpeg",
+            "https://i.imgur.com/F8vhnFJ.jpeg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 40,
+        "title": "Futuristic Silver and Gold High-Top Sneaker",
+        "slug": "futuristic-silver-and-gold-high-top-sneaker",
+        "price": 68,
+        "description": "Step into the future with this eye-catching high-top sneaker, designed for those who dare to stand out. The sneaker features a sleek silver body with striking gold accents, offering a modern twist on classic footwear. Its high-top design provides support and style, making it the perfect addition to any avant-garde fashion collection. Grab a pair today and elevate your shoe game!",
+        "category": {
+            "id": 4,
+            "name": "Shoes",
+            "slug": "shoes",
+            "image": "https://i.imgur.com/qNOjJje.jpeg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/npLfCGq.jpeg",
+            "https://i.imgur.com/vYim3gj.jpeg",
+            "https://i.imgur.com/HxuHwBO.jpeg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 41,
+        "title": "Futuristic Chic High-Heel Boots",
+        "slug": "futuristic-chic-high-heel-boots",
+        "price": 36,
+        "description": "Elevate your style with our cutting-edge high-heel boots that blend bold design with avant-garde aesthetics. These boots feature a unique color-block heel, a sleek silhouette, and a versatile light grey finish that pairs easily with any cutting-edge outfit. Crafted for the fashion-forward individual, these boots are sure to make a statement.",
+        "category": {
+            "id": 4,
+            "name": "Shoes",
+            "slug": "shoes",
+            "image": "https://i.imgur.com/qNOjJje.jpeg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/HqYqLnW.jpeg",
+            "https://i.imgur.com/RlDGnZw.jpeg",
+            "https://i.imgur.com/qa0O6fg.jpeg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 42,
+        "title": "Elegant Patent Leather Peep-Toe Pumps with Gold-Tone Heel",
+        "slug": "elegant-patent-leather-peep-toe-pumps",
+        "price": 53,
+        "description": "Step into sophistication with these chic peep-toe pumps, showcasing a lustrous patent leather finish and an eye-catching gold-tone block heel. The ornate buckle detail adds a touch of glamour, perfect for elevating your evening attire or complementing a polished daytime look.",
+        "category": {
+            "id": 4,
+            "name": "Shoes",
+            "slug": "shoes",
+            "image": "https://i.imgur.com/qNOjJje.jpeg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/AzAY4Ed.jpeg",
+            "https://i.imgur.com/umfnS9P.jpeg",
+            "https://i.imgur.com/uFyuvLg.jpeg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 43,
+        "title": "Elegant Purple Leather Loafers",
+        "slug": "elegant-purple-leather-loafers",
+        "price": 17,
+        "description": "Step into sophistication with our Elegant Purple Leather Loafers, perfect for making a bold statement. Crafted from high-quality leather with a vibrant purple finish, these shoes feature a classic loafer silhouette that's been updated with a contemporary twist. The comfortable slip-on design and durable soles ensure both style and functionality for the modern man.",
+        "category": {
+            "id": 4,
+            "name": "Shoes",
+            "slug": "shoes",
+            "image": "https://i.imgur.com/qNOjJje.jpeg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/Au8J9sX.jpeg",
+            "https://i.imgur.com/gdr8BW2.jpeg",
+            "https://i.imgur.com/KDCZxnJ.jpeg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 44,
+        "title": "Classic Blue Suede Casual Shoes",
+        "slug": "classic-blue-suede-casual-shoes",
+        "price": 39,
+        "description": "Step into comfort with our Classic Blue Suede Casual Shoes, perfect for everyday wear. These shoes feature a stylish blue suede upper, durable rubber soles for superior traction, and classic lace-up fronts for a snug fit. The sleek design pairs well with both jeans and chinos, making them a versatile addition to any wardrobe.",
+        "category": {
+            "id": 4,
+            "name": "Shoes",
+            "slug": "shoes",
+            "image": "https://i.imgur.com/qNOjJje.jpeg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/sC0ztOB.jpeg",
+            "https://i.imgur.com/Jf9DL9R.jpeg",
+            "https://i.imgur.com/R1IN95T.jpeg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 45,
+        "title": "Sleek Futuristic Electric Bicycle",
+        "slug": "sleek-futuristic-electric-bicycle",
+        "price": 22,
+        "description": "This modern electric bicycle combines style and efficiency with its unique design and top-notch performance features. Equipped with a durable frame, enhanced battery life, and integrated tech capabilities, it's perfect for the eco-conscious commuter looking to navigate the city with ease.",
+        "category": {
+            "id": 5,
+            "name": "Miscellaneous",
+            "slug": "miscellaneous",
+            "image": "https://i.imgur.com/BG8J0Fj.jpg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/BG8J0Fj.jpg",
+            "https://i.imgur.com/ujHBpCX.jpg",
+            "https://i.imgur.com/WHeVL9H.jpg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 46,
+        "title": "Sleek All-Terrain Go-Kart",
+        "slug": "sleek-all-terrain-go-kart",
+        "price": 37,
+        "description": "Experience the thrill of outdoor adventures with our Sleek All-Terrain Go-Kart, featuring a durable frame, comfortable racing seat, and robust, large-tread tires perfect for handling a variety of terrains. Designed for fun-seekers of all ages, this go-kart is an ideal choice for backyard racing or exploring local trails.",
+        "category": {
+            "id": 5,
+            "name": "Miscellaneous",
+            "slug": "miscellaneous",
+            "image": "https://i.imgur.com/BG8J0Fj.jpg",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/Ex5x3IU.jpg",
+            "https://i.imgur.com/z7wAQwe.jpg",
+            "https://i.imgur.com/kc0Dj9S.jpg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    },
+    {
+        "id": 47,
+        "title": "Radiant Citrus Eau de Parfum",
+        "slug": "radiant-citrus-eau-de-parfum",
+        "price": 73,
+        "description": "Indulge in the essence of summer with this vibrant citrus-scented Eau de Parfum. Encased in a sleek glass bottle with a bold orange cap, this fragrance embodies freshness and elegance. Perfect for daily wear, it's an olfactory delight that leaves a lasting, zesty impression.",
+        "category": {
+            "image": "https://i.imgur.com/BG8J0Fj.jpg",
+            "id": 5,
+            "name": "Miscellaneous",
+            "slug": "miscellaneous",
+            "creationAt": "2025-09-27T11:02:07.000Z",
+            "updatedAt": "2025-09-27T11:02:07.000Z"
+        },
+        "images": [
+            "https://i.imgur.com/xPDwUb3.jpg",
+            "https://i.imgur.com/3rfp691.jpg",
+            "https://i.imgur.com/kG05a29.jpg"
+        ],
+        "creationAt": "2025-09-27T11:02:07.000Z",
+        "updatedAt": "2025-09-27T11:02:07.000Z"
+    }
+]
+
+const container = document.querySelector('.containerr')
+
+products.forEach(item => {
+    const cart = document.createElement("div")
+    cart.classList.add("product_cart")
+    cart.innerHTML = `
+    <img src=${item.category.image} alt=${item.title} width="200" height="200"
+    <h1>${item.title}</h1>
+    <p>${item.description}</p>
+    <p>${item.price}<p/>
+    `
+    container.appendChild(cart)
+})
+
+
+
+
